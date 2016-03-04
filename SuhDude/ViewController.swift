f878fd6a00fe050bea3fdc869ae30cd265684bc1
vik denic <vik.denic@gmail.com>
2016-03-03 18:07:00 -0600

    set up Backendless SDK

--- SuhDude/ViewController.swift
+++ SuhDude/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let backendless = Backendless.sharedInstance()
+    let user: BackendlessUser = BackendlessUser()
+    user.email = "michael@backendless.com"
+    user.password = "my_super_password"
+    backendless.userService.registering(user)
+  }
 
 }
 
