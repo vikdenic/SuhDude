a7b25cc099982e147e3d0b5e287252f2dfc069a7
vik denic <vik.denic@gmail.com>
2016-03-05 10:26:59 -0600

    register UI

---
+++ SuhDude/RegisterUserViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterUserViewController.swift
+//  SuhDude
+//
+//  Created by Vik Denic on 3/5/16.
+//  Copyright © 2016 nektur labs. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+  @IBOutlet var usernameTextField: UITextField!
+  @IBOutlet var passwordTextField: UITextField!
+  @IBOutlet var continueButton: UIButton!
+
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  @IBAction func onContinueButtonTapped(sender: UIButton) {
+
+  }
+
+}
---
+++ SuhDude/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  SuhDude
+//
+//  Created by Vik Denic on 3/5/16.
+//  Copyright © 2016 nektur labs. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
