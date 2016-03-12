879d90e65ba996b2d0a445bd41db78cb79ba37b8
vik denic <vik.denic@gmail.com>
2016-03-11 20:52:31 -0600

    add businesslogic

---
+++ BusinessLogic/src/com/backendless/suhdude/Bootstrap.java
@@ -0,0 +1,27 @@
+
+package com.backendless.suhdude;
+
+import com.backendless.Backendless;
+import com.backendless.servercode.IBackendlessBootstrap;
+
+
+public class Bootstrap implements IBackendlessBootstrap
+{
+            
+  @Override
+  public void onStart()
+  {
+    Backendless.setUrl( "https://api.backendless.com" );
+    Backendless.initApp( "FCC9E18B-C20B-0C59-FFE0-E931AC63F400", "5F9DA2B2-ADDB-0DF3-FF82-9A993B1DD700","v1");
+
+    // add your code here
+  }
+    
+  @Override
+  public void onStop()
+  {
+    // add your code here
+  }
+    
+}
+        
---
+++ BusinessLogic/src/com/backendless/suhdude/events/user_service/GenericUserEventHandler.java
@@ -0,0 +1,38 @@
+package com.backendless.suhdude.events.user_service;
+
+import com.backendless.servercode.RunnerContext;
+
+import java.util.HashMap;
+        
+/**
+* GenericUserEventHandler handles the User Service events.
+* The event handlers are the individual methods implemented in the class.
+* The "before" and "after" prefix determines if the handler is executed before
+* or after the default handling logic provided by Backendless.
+* The part after the prefix identifies the actual event.
+* For example, the "beforeLogin" method is the "Login" event handler and will
+* be called before Backendless applies the default login logic. The event
+* handling pipeline looks like this:
+
+* Client Request ---> Before Handler ---> Default Logic ---> After Handler --->
+* Return Response
+*/
+public class GenericUserEventHandler extends com.backendless.servercode.extension.UserExtender
+{
+    
+  @Override
+  public void beforeRegister( RunnerContext context, HashMap userValue ) throws Exception
+  {
+//    userValue.put("name", userValue.get("name").toString().toLowerCase());
+//    userValue.put("password", userValue.get("password").toString().toLowerCase());
+  }
+
+  @Override
+  public void beforeLogin( RunnerContext context, String login, String password ) throws Exception
+  {
+//    login = login.toLowerCase();
+//    password = password.toLowerCase();
+  }
+    
+}
+        
