856c1371397060045ff2b8219014d678e428707e
vik denic <vik.denic@gmail.com>
2016-03-26 01:22:20 -0500

    create settings vc

---
+++ SuhDude/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  SuhDude
+//
+//  Created by Vik Denic on 3/26/16.
+//  Copyright © 2016 nektur labs. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
